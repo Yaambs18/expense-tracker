bc8cb936769ec6c627aee3d7fe13e9a32a03f8af
yanshbhardwaj14@gmail.com <yanshbhardwaj14@gmail.com>
2023-12-31 19:47:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's get started!</h1>
+      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>Expense Item!</h2>
+            <li>Food Rs 10</li>
+            <li>Petrol Rs 100</li>
+            <li>Movies Rs 200</li>
+        </div>
+    )
+}
+
+export default ExpenseItem;

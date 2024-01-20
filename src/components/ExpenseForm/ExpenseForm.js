26d3385b6a80ad29018943b7afad3876255169d1
Yaambs18 <yanshbhardwaj14@gmail.com>
2024-01-20 16:27:58 +0530

    handling expense form input and submit event using state

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,31 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-    const titleChangeHandler = (event) => {
-        console.log(event.target.value);
+
+    const expenseObj = {
+        title: '',
+        amount: '',
+        location: '',
+        date: '',
     }
+
+    // const [enteredTitle, setTitle] = useState(expenseObj.title);
+    // const [enteredAmount, setAmount] = useState(expenseObj.amount);
+    // const [enteredLocation, setLocation] = useState(expenseObj.location);
+    // const [enteredDate, setDate] = useState(expenseObj.date);
+    const [enteredExpense, setExpense] = useState(expenseObj);
+
+    const titleChangeHandler = (event) => {
+        // console.log(event.target.value);
+        const newTitle = event.target.value;
+        // setTitle(newTitle);
+        // setExpense({
+        //     ...enteredExpense,
+        //     title: newTitle
+        // })
+        setExpense((prevState) => {
+            return { ...prevState, title: newTitle };
+        })
+    };
+    const amountChangeHandler = (event) => {
+        // console.log(event.target.value);
+        const newAmount = event.target.value;
+        // setAmount(newAmount);
+        setExpense({
+            ...enteredExpense,
+            amount: newAmount
+        })
+    };
+    const locationChangeHandler = (event) => {
+        // console.log(event.target.value);
+        const newLocation = event.target.value;
+        // setLocation(newLocation);
+        setExpense({
+            ...enteredExpense,
+            location: newLocation
+        })
+    };
+    const dateChangeHandler = (event) => {
+        // console.log(event.target.value);
+        const newDate = event.target.value;
+        // setDate(newDate);
+        setExpense({
+            ...enteredExpense,
+            date: newDate
+        })
+    }
+
+    const submitFormHandler = (event) => {
+        event.preventDefault();
+        console.log(enteredExpense);
+    }
+
     return (
         <form>
             <div className="expense-form">
                 <div className="form-item">
                     <label>Title: </label>
-                    <input placeholder="Title" id="title" onChange={titleChangeHandler}/>
+                    <input placeholder="Title" id="title" onChange={titleChangeHandler} />
                 </div>
                 <div className="form-item">
                     <label>Amount: </label>
-                    <input placeholder="Amount" type="number" min="0.01" step="0.01" id="amount"/>
+                    <input placeholder="Amount" type="number" min="0.01" step="0.01" id="amount" onChange={amountChangeHandler}/>
                 </div>
                 <div className="form-item">
                     <label>Location: </label>
-                    <input placeholder="Location" id="location"/>
+                    <input placeholder="Location" id="location"  onChange={locationChangeHandler}/>
                 </div>
                 <div className="form-item">
                     <label>Date: </label>
-                    <input placeholder="Date" id="date" type="date"/>
+                    <input placeholder="Date" id="date" type="date" onChange={dateChangeHandler} />
                 </div>
                 <div className="add-expense">
-                    <button id="submitBtn" type="submit">Add Expense</button>
+                    <button id="submitBtn" type="submit" onClick={submitFormHandler}>Add Expense</button>
                 </div>
             </div>
         </form>

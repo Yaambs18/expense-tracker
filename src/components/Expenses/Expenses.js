45637552b2b704deca40b82af1b1119170a82e58
Yaambs18 <yanshbhardwaj14@gmail.com>
2024-01-21 18:03:05 +0530

    Adding a filter (non-functional)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css'
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
     const expenseComp = [];
     for (const expense of props.expenses) {
       expenseComp.push(<ExpenseItem key={expense.id} expense={expense}></ExpenseItem>);
     }
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+      setFilteredYear(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             {expenseComp}
         </Card>
     )
